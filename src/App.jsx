0ac79d1b87352191198d49e5cca2864710fb25ed
bowen-anth <bowen.anth@gmail.com>
2023-11-28 18:06:01 -0700

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 import './App.css'
+import {encode} from 'html-entities';
+
+encode('< > " \' & © ∆');
+// -> '&lt; &gt; &quot; &apos; &amp; © ∆'
+
+encode('< ©', {mode: 'nonAsciiPrintable'});
+// -> '&lt; &copy;'
+
+encode('< ©', {mode: 'nonAsciiPrintable', level: 'xml'});
+// -> '&lt; &#169;'
+
+encode('< > " \' & ©', {mode: 'nonAsciiPrintableOnly', level: 'xml'});
+// -> '< > " \' & &#169;'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [questions, setQuestions] = React.useState([])
+  
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+        .then(res => res.json())
+        .then(data => {
+            setQuestions(data.results)
+            console.log(data)
+        })
+        .catch(error => {
+            console.error("There was an error:", error)
+    })
+}, [])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+      <>
+
+      </>
+    )
 }
 
 export default App
